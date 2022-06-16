c6b8afb1d1fba3d967c3448794ac977b390ec75f
Thalles Campagnani <thallescampagnani@gmail.com>
2022-06-16 00:05:46 -0300

    Delete lxqt2lxde.sh

--- lxqt2lxde.sh
+++
@@ -1,5 +0,0 @@
-#Instalar LXDE
-sudo apt install lxde arc-theme deepin-icon-theme -y
-
-#Remover programas do LXQt
-sudo apt autoremove lxqt lxqt-core lubuntu-desktop lximage-qt lxqt-config lxqt-openssh-askpass lxqt-powermanagement lxqt-session-l10n lxqt-themes lxqt-config-l10n lxqt-openssh-askpass-l10n lxqt-powermanagement-l10n lxqt-sudo lxqt-notificationd lxqt-policykit lxqt-qtplugin lxqt-sudo-l10n lxqt-notificationd-l10n lxqt-policykit-l10n lxqt-session lxqt-system-theme libreoffice-common libreoffice-help-common libreoffice-style-breeze libreoffice-core libreoffice-help-en-us libreoffice-style-colibre qpdfview qterminal deluge gnome-disk-utility parcellite lxmusic -y
